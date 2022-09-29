0787a42437b5f89d127cd480b6afc7dca9b9b685
akashahmmed303 <ahmedakash336@gmail.com>
2022-09-29 21:04:39 +0600

    cart added done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Product.css';
 const Product = props => {
-  const { name, img, forAge, timeRequired } = props.product;
+  const { name, img, forAge, timeRequired, para } = props.product;
   return (
     <div className='product'>
       <img src={img} alt='' />
-      <h6 className='product-name'>{name}</h6>
+      <div className='product-info'>
+        <h6 className='product-name'>{name}</h6>
+        <p>{para}</p>
+        <p className='p'>For Age : {forAge}</p>
+        <p className='p'>Time required : {timeRequired}</p>
+      </div>
+      <button className='btn-cart'>
+        <p>Add to List</p>
+      </button>
     </div>
   );
 };
