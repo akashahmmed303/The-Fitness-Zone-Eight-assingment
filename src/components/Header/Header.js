c100248546ac1bba65482a9085c60c7ad2ff6ca2
akashahmmed303 <ahmedakash336@gmail.com>
2022-09-29 15:44:00 +0600

    header done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img
+        src='https://images-platform.99static.com/MZXWaRAM0hdFsoiV5rl9WUctJq0=/500x500/top/smart/99designs-contests-attachments/57/57274/attachment_57274394'
+        alt=''
+      />
+      <h1>The Fitness Zone</h1>
+    </div>
+  );
+};
+
+export default Header;
